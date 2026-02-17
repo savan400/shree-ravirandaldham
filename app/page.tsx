86361f96bf2c2c645db5c9db29870677023fabbb
savan400 <savan.k.neoverce@gmail.com>
2026-02-17 16:26:19 +0530

    - build errors fixed - ui components cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import Image from "next/image";
-import Header from "./components/Header";
-import HeroBanner from "./components/HeroBanner";
-import TypingText from "./components/TypingText";
-import Announcements from "./components/Announcements";
-import KashtabhanjanDev from "./components/KashtabhanjanDev";
-import KingOfSalangpur from "./components/KingOfSalangpur";
-import LiveDarshan from "./components/LiveDarshan";
-import DailyDarshan from "./components/DailyDarshan";
-import FacilitiesCards from "./components/FacilitiesCards";
-import StatsSection from "./components/StatsSection";
-import HariVani from "./components/HariVani";
-import Footer from "./components/Footer";
-import DarshanTimingsSidebar from "./components/DarshanTimingsSidebar";
+import Announcements from "@/components/Announcements/Announcements";
+import DailyDarshan from "@/components/DailyDarshan/DailyDarshan";
+import DarshanTimingsSidebar from "@/components/DarshanTimingsSidebar";
+import FacilitiesCards from "@/components/FacilitiesCards/FacilitiesCards";
+import Footer from "@/components/Footer";
+import HariVani from "@/components/HariVani/HariVani";
+import Header from "@/components/Header";
+import HeroBanner from "@/components/HeroBanner/HeroBanner";
+import KashtabhanjanDev from "@/components/KashtabhanjanDev/KashtabhanjanDev";
+import KingOfSalangpur from "@/components/KingOfSalangpur/KingOfSalangpur";
+import LiveDarshan from "@/components/LiveDarshan/LiveDarshan";
+import StatsSection from "@/components/StatsSection/StatsSection";
+import TypingText from "@/components/TypingText/TypingText";
+
 
 export default function Home() {
   return (
