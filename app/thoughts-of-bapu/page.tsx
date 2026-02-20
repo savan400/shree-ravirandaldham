ab6c24ad0c230c3cb4438f299e290eef463f5391
savan400 <savan.k.neoverce@gmail.com>
2026-02-20 16:05:32 +0530

    Refactor code structure for improved readability and maintainability

---
+++ app/thoughts-of-bapu/page.tsx
@@ -0,0 +1,12 @@
+import ThoughtsOfBapuPage from '@/page-ui/ThoughtsOfBapuPage'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <ThoughtsOfBapuPage />
+        </div>
+    )
+}
+
+export default page
